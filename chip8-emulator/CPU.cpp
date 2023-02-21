48a0094edc4cbd4c5d69330f55333a3b12098a5e
Aaron Russell <aaronrussell457@gmail.com>
2023-02-21 00:51:34 -0500

    Add 5xy0 opcode

--- chip8-emulator/CPU.cpp
+++ chip8-emulator/CPU.cpp
@@ -91,6 +91,9 @@ void CPU::decodeOpcode(uint16_t opcode)
 	case 0x4:
 		OP_4xkk(opcode);
 		break;
+	case 0x5:
+		OP_5xy0(opcode);
+		break;
 	}
 }
 
@@ -136,4 +139,11 @@ void CPU::OP_4xkk(uint16_t opcode)
 	uint8_t vx = (opcode & 0x0F00) >> 8;
 	uint8_t value = opcode & 0x00FF;
 	if (registers[vx] != value) programCounter += 2;
+}
+
+void CPU::OP_5xy0(uint16_t opcode)
+{
+	uint8_t vx = (opcode & 0x0F00) >> 8;
+	uint8_t vy = (opcode & 0x00F0) >> 4;
+	if (registers[vx] == registers[vy]) programCounter += 2;
 }
--- chip8-emulator/CPU.h
+++ chip8-emulator/CPU.h
@@ -62,4 +62,5 @@ private:
 	void OP_2nnn(uint16_t opcode);
 	void OP_3xkk(uint16_t opcode);
 	void OP_4xkk(uint16_t opcode);
+	void OP_5xy0(uint16_t opcode);
 };
